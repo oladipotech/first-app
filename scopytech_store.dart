9a48e719ea3326c4deb1e81fad86be2dbe16ceef
Scopytech <46828543+oladipotech@users.noreply.github.com>
2020-03-29 03:18:17 +0100

    Update scopytech_store.dart

--- scopytech_store.dart
+++ scopytech_store.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      title: "my flutter app",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('scopytechnology store'),
-        ),
-        body: Material(
-        color: Colors.lightGreenAccent
-          child: Center(
-            child: Text('kamil yasir', textDirection: TextDirection.ltr,),
-          ),
-    )
-    ));
-  }
+class MemberProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  LBAppBar().getAppBar(),
+                  drawer:  LBDrawer().getDrawer(),
+                              body: ListView(
+                                children: <Widget>[
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                        colors: [Color.fromRGBO(1, 89, 99, 1.0), Colors.grey],
+                                        begin: Alignment.bottomLeft,
+                                        end: Alignment.topRight,
+                                      ),
+                                    ),
+                                    child: Container(
+                                      child: Column(
+                                        children: <Widget>[
+                                          Container(
+                                            margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                                            child: Text("Name : Kamil Yasir",
+                                                style: TextStyle(
+                                                    color: Colors.white70,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 19.0)),
+                                          ),
+                                          Container(
+                                              margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                                              child: Text("Username : scopytech",
+                                                  style: TextStyle(
+                                                      color: Colors.white70,
+                                                      fontWeight: FontWeight.bold,
+                                                      fontSize: 19.0))),
+                                          Container(
+                                              margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                                              child: Text("slack mail : scopytechnology@gmail.com",
+                                                  style: TextStyle(
+                                                      color: Colors.white70,
+                                                      fontWeight: FontWeight.bold,
+                                                      fontSize: 19.0))),
+                                          Container(
+                                              margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                                              child: Text("track : front-end, mobile & flutter",
+                                                  style: TextStyle(
+                                                      color: Colors.white70,
+                                                      fontWeight: FontWeight.bold,
+                                                      fontSize: 19.0))),
+                                        ],//<Widget>[]
+                                      ),//column
+                                    ),//container
+                                    //here
+                                  ),//container
+                                ],//<Widget>[]
+                              ),//listview
+                            );//scaffold
+                          }
+                        }
+            
+            class LBAppBar {
+        getAppBar() {}
+}
+            
+            class LBDrawer {
+        getDrawer() {}
+}
